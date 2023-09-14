483c2189ac3b74f90f9138f89f3e8534608e51a3
Braxton Rider <126611124+SirRexOfRider@users.noreply.github.com>
2023-09-14 11:22:16 -0600

    Ok now it's there

--- Crazy_1.java
+++ Crazy_1.java
@@ -1,3 +1,67 @@
+/*
+ * Name:Crazy_1.java
+ * Author:Rex
+ * Date:9/13/23
+ * Purpose:Go crazy
+ */
+
+
 public class Crazy_1 {
-    
+    public static void main(String[] args) {
+        while (true) {
+
+        //Code I found to delay code
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ie) {
+            Thread.currentThread().interrupt();
+        }
+        
+        System.out.println("Crazy?");
+        System.out.println();
+          try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ie) {
+            Thread.currentThread().interrupt();
+        }
+
+        System.out.println("I was crazy once...");
+        System.out.println();
+          try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ie) {
+            Thread.currentThread().interrupt();
+        }
+
+        System.out.println("They locked me in a room");
+        System.out.println();
+          try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ie) {
+            Thread.currentThread().interrupt();
+        }
+
+        System.out.println("A rubber room");
+        System.out.println();
+          try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ie) {
+            Thread.currentThread().interrupt();
+        }
+
+        System.out.println("A rubber room with rats");
+        System.out.println();
+          try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ie) {
+            Thread.currentThread().interrupt();
+        }
+        
+        System.out.println("And rats make me crazy...");
+        System.out.println();
+
+        
+        
+        }
+    }
 }
